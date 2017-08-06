3a29b623b6e2b0f6addadd813ad7c82e0cf7419b
Edward Hobbs <eh362@kent.ac.uk>
2017-08-06 12:04:17 +0100

    initial commit

---
+++ lib/advent_of_code_2015.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2015 do
+  @moduledoc """
+  Documentation for AdventOfCode2015.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2015.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code_2015_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2015Test do
+  use ExUnit.Case
+  doctest AdventOfCode2015
+
+  test "greets the world" do
+    assert AdventOfCode2015.hello() == :world
+  end
+end
