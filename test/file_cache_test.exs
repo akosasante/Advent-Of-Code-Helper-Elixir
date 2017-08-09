ccaf88fe8584bf36638a69e81b600cbacff3be4c
Edward Hobbs <eh362@kent.ac.uk>
2017-08-09 23:08:51 +0100

    added warnings back

--- test/file_cache_test.exs
+++ test/file_cache_test.exs
@@ -2,7 +2,7 @@ defmodule FileCacheTest do
   use ExUnit.Case
 
   setup _context do
-    cache_dir = Application.get_env(:advent_of_code, :cache_dir) |> Path.join("test")
+    cache_dir = Application.get_env(:advent_of_code, :cache_dir)
     File.mkdir cache_dir
     File.write(Path.join(cache_dir,"input_test_day"),"test contents",[])
 
