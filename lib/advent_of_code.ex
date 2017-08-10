186b9c5ad5659250ff2043461289e9f577044cca
Edward Hobbs <eh362@kent.ac.uk>
2017-08-10 20:16:37 +0100

    added default year option

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -13,4 +13,15 @@ defmodule AdventOfCode do
   def get_input(year,day) do
     GetInputs.get_value(year,day,Application.get_env(:advent_of_code, :session))
   end
+
+  @doc """
+  Return input values for current year
+
+  ## Parameters
+    - day: Int that represents day of puzzle
+  """
+  def get_input(day) do
+    today = Date.utc_today()
+    get_input(today.year, day)
+  end
 end
