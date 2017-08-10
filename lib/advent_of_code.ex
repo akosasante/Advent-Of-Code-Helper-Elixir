8b348254a268590e105948b1dce67f63f6df1423
Edward Hobbs <eh362@kent.ac.uk>
2017-08-10 21:01:47 +0100

    Add default year (#5)
    * added default year option
    
    * changed default year to get latest valid year, rather than just current year
    
    * module tests weren't tracked
    
    * use different cassette so travis is happy

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -13,4 +13,22 @@ defmodule AdventOfCode do
   def get_input(year,day) do
     GetInputs.get_value(year,day,Application.get_env(:advent_of_code, :session))
   end
+
+  @doc """
+  Return input values for most recent year
+
+  ## Parameters
+    - day: Int that represents day of puzzle
+  """
+  def get_input(day) do
+    calculate_year() |> get_input(day)
+  end
+
+  defp calculate_year do
+    today = Date.utc_today
+    case today.month < 12 do
+      true -> today.year-1
+      false -> today.year
+    end
+  end
 end
---
+++ test/advent_of_code_test.exs
@@ -0,0 +1,32 @@
+defmodule AdventOfCodeTest do
+  use ExUnit.Case, async: false
+  use ExVCR.Mock
+
+  setup_all do
+    ExVCR.Config.cassette_library_dir("fixture/vcr_cassettes")
+    :ok
+  end
+
+
+  test "do we get the correct value" do
+    use_cassette("everything_should_work") do
+      {:ok, contents} = AdventOfCode.get_input(2015,1)
+      assert String.length(contents) == 7000
+    end
+  end
+
+  test "gets current year correctly" do
+    use_cassette("most_recent_year") do
+      {:ok, _contents} = AdventOfCode.get_input(1)
+      assert File.exists?(".cache/input_#{calculate_year()}_1")
+    end
+  end
+
+  defp calculate_year do
+    today = Date.utc_today
+    case today.month < 12 do
+      true -> today.year-1
+      false -> today.year
+    end
+  end
+end
