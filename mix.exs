e3e2e0f60391fe34fe5f4cea81460aeb3d12a20e
Edward Hobbs <eh362@kent.ac.uk>
2017-08-11 22:01:14 +0100

    increment version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AdventOfCode.Mixfile do
   def project do
     [
       app: :advent_of_code_helper,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
