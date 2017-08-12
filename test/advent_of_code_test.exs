3f5acf0e38ba03cd4ebe3b6b8001dc6a4f7b2d81
Edward Hobbs <eh362@kent.ac.uk>
2017-08-12 22:29:20 +0100

    updated module test to use GetInputs stub

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -2,8 +2,12 @@ defmodule AdventOfCodeHelperTest do
   use ExUnit.Case
 
   test "do we get the correct value" do
+    stub = GetInputs
+           |> double
+           |> allow(:get_value, &({:ok, &3}))
     {:ok, contents} = AdventOfCodeHelper.get_input(2015,1)
-    assert String.length(contents) == 7000
+    assert contents == Application.get_env(:advent_of_code, :session)
+    assert_receive(:example, 2015}
   end
 
 end
