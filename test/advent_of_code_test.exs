4fc916f18d130a900f25fcb4859b88bd6e311530
Edward Hobbs <eh362@kent.ac.uk>
2017-08-12 22:35:38 +0100

    fixed stupid errors, also turns out you can't use anon syntax like that so back to the basic way i go

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -1,13 +1,14 @@
 defmodule AdventOfCodeHelperTest do
   use ExUnit.Case
+  import Double
 
   test "do we get the correct value" do
     stub = GetInputs
            |> double
-           |> allow(:get_value, &({:ok, &3}))
-    {:ok, contents} = AdventOfCodeHelper.get_input(2015,1)
+           |> allow(:get_value, fn(_y,_d,session,_hm) -> {:ok, session} end)
+    {:ok, contents} = AdventOfCodeHelper.get_input(2015,1,stub)
     assert contents == Application.get_env(:advent_of_code, :session)
-    assert_receive(:example, 2015}
+    assert_receive(:example, 2015)
   end
 
 end
