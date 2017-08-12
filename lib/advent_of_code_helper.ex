63c549049548631632691832bb33b65fe26e9ef0
Edward Hobbs <eh362@kent.ac.uk>
2017-08-12 22:20:37 +0100

    added ability to stub GetInputs

--- lib/advent_of_code_helper.ex
+++ lib/advent_of_code_helper.ex
@@ -11,8 +11,8 @@ defmodule AdventOfCodeHelper do
     - year: Int that represents year of puzzle
     - day: Int that represents day of puzzle
   """
-  def get_input(year,day) do
-    GetInputs.get_value(year,day,Application.get_env(:advent_of_code_helper, :session))
+  def get_input(year,day, get_mod \\ GetInputs) do
+    get_mod.get_value(year,day,Application.get_env(:advent_of_code_helper, :session))
   end
 
 end
